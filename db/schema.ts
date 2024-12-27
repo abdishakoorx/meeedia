e36702908bcd07c58d49e505e3a0b491930ba30b
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-12-27 18:15:48 +0300

    refactor: change description field type from json to jsonb for improved performance

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, json, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, jsonb, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const USER_TABLE = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -12,7 +12,7 @@ export const VIDEO_RAW_TABLE = pgTable("video_raw", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   videoID: varchar().notNull(),
   title: varchar({ length: 255 }).notNull(),
-  description: json(),
+  description: jsonb(),
   videoType: varchar({ length: 255 }),
   createdBy: varchar().notNull().references(() => USER_TABLE.email),
 });
